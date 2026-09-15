3080dfe8e86c7156bd977524e302c6feda7d681f
Joseph Joy <josephjoyofficial@gmail.com>
2026-09-15 23:37:07 +0530

    Add: pascals-triangle - submission-1

---
+++ Data Structures & Algorithms/pascals-triangle/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = []
+        for num in range(numRows):
+            if num == 0:
+                result.append([1])
+            elif num == 1:
+                result.append([1,1])
+            else:
+                prev_row = result[len(result) - 1]
+                curr_num = num + 1
+                new_row = [None] * curr_num
+                curr_sum = 0
+                for idx,row_val in enumerate(prev_row):
+                    new_row[idx] = curr_sum + row_val
+                    if idx / 2 == 0:
+                        curr_sum += row_val
+                    else:
+                        curr_sum = row_val
+                new_row[curr_num-1] = 1
+                result.append(new_row)
+        return result
