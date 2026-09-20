f7c3e881f5c5a72d11fb80d1bce57515383affc7
Joseph Joy <josephjoyofficial@gmail.com>
2026-09-20 13:45:43 +0530

    Add: valid-palindrome-ii - submission-4

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def __init__(self):
+        self.of_used = False
+
+    def validPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if s[l] == s[r]:
+                l += 1
+                r -= 1
+                continue
+            elif not self.of_used:
+                self.of_used = True
+                lres = self.validPalindrome(s[l:r])
+                rres = self.validPalindrome(s[l+1:r+1])
+                return lres or rres
+            else:
+                return False
+        return True
+                
